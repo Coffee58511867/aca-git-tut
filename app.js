b043e167c99645eca79290465d19ed6f82f642cc
Les Chinyanga <les.chinyanga@africacode.academy>
2022-07-28 13:23:43 +0200

    Add informitive comments

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log('hello world');
 
+// git status - check the status of the repo
+// git add <filename> - add file to the staging area
+// git commit -m "<message>"
+// "the purpose of this commit is to... <message>"
+
 console.log('hello from git');
