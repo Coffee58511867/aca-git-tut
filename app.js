0ef14e115d82cd5efd62e58637e5cb22c407355b
Les Chinyanga <les.chinyanga@africacode.academy>
2022-07-28 13:37:34 +0200

    Add git logs

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ console.log('hello world');
 
 // git status - check the status of the repo
 // git add <filename> - add file to the staging area
+// git add . - add all unstaged changes
 // git commit -m "<message>"
 // "the purpose of this commit is to... <message>"
+// git log - view commits
+// git log --oneline
 
 console.log('hello from git');
 console.log('asdfasdf');
