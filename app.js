f82e0e3390ce8d7c484209cb61502900689b9841
Les Chinyanga <les.chinyanga@africacode.academy>
2022-07-28 13:55:44 +0200

    Add git config instrcutions

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ console.log('hello world');
 // git config --global user.name "FirstName LastName"
 // git config --global user.email "youremail@fake.com"
 // git config --global --edit - open the config file
+// git config --global core.editor "code --wait" - open git config in vscode
+// when adding an alias give the full command minus git (see alias.txt)
 
 console.log('hello from git');
 console.log('asdfasdf');
